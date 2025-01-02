09456ae4ef0f11320eabfb5cd222ee0cb49a987c
Ryansyah <officialteam548@gmail.com>
2025-01-02 19:14:25 +0700

    part 29

--- lib/data/code_con_repository.dart
+++ lib/data/code_con_repository.dart
@@ -1,18 +1,20 @@
-import 'package:webinar_fe/data/domain/duitku_entities/get_payment_method_params.dart';
-import 'package:webinar_fe/data/domain/entities/payment_method.dart';
-import 'package:webinar_fe/data/domain/entities/tresult.dart';
+import 'package:webinar_fe/domain/duitku_entities/create_reservation_params.dart';
+import 'package:webinar_fe/domain/duitku_entities/get_payment_method_params.dart';
+import 'package:webinar_fe/domain/entities/payment_method.dart';
+import 'package:webinar_fe/domain/entities/reservation.dart';
+import 'package:webinar_fe/domain/entities/tresult.dart';
 import 'package:dio/dio.dart';
 
 class CodeConRepository {
   final Dio _dio;
-  final String baseUrl = 'code-con-course-be.globeapp.dev';
+  final String baseUrl = 'https://webinar-be-9d58.globeapp.dev';
 
   CodeConRepository({Dio? dio}) : _dio = dio ?? Dio();
 
   Future<TResult<List<PaymentMethod>>> getPaymentMethods(
       {GetPaymentMethodParams? params}) async {
     try {
-      final response = await _dio.post('https://$baseUrl/getPaymentMethods',
+      final response = await _dio.post('$baseUrl/getPaymentMethods',
           data: params?.toJson() ?? {});
 
       final paymentMethods = (response.data['paymentFee'] as List)
@@ -23,31 +25,26 @@ class CodeConRepository {
 
       return Success(paymentMethods);
     } on DioException catch (e) {
-      return Failure(e.response?.data.toString() ?? e.message.toString());
+      return Failure('${e.response?.data ?? e.message}');
     }
   }
 
-  Future<String> testMakingReservation() async {
+  Future<TResult<Reservation>> createReservation(
+      {required CreateReservationParams params}) async {
     try {
       final response =
-          await _dio.post('https://$baseUrl/makingReservation', data: {
-        "paymentMethod": "VA",
-        "orderId": "ORD-123999",
-        "customerName": "John Rambo",
-        "customerEmail": "rambo@blackpink.com",
-        "returnUrl": "",
-        "totalPayment": 250000,
-        "orderDescription": "CodeCon Ticket"
-      });
-      return response.data.toString();
+          await _dio.post('$baseUrl/makingReservation', data: params.toJson());
+
+      final reservation = Reservation.fromJson(response.data['data']);
+      return Success(reservation);
     } on DioException catch (e) {
-      return 'Error: ${e.message}';
+      return Failure('${e.response?.data ?? e.message}');
     }
   }
 
   Future<String> testCheckReservation() async {
     try {
-      final response = await _dio.post('https://$baseUrl/checkReservation',
+      final response = await _dio.post('$baseUrl/checkReservation',
           data: {'email': 'rambo@blackpink.com'});
       return response.data.toString();
     } on DioException catch (e) {
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'dart:developer';
 
 import 'package:webinar_fe/data/code_con_repository.dart';
-import 'package:webinar_fe/data/domain/entities/tresult.dart';
+import 'package:webinar_fe/domain/duitku_entities/create_reservation_params.dart';
+import 'package:webinar_fe/domain/entities/tresult.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,12 +14,25 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
             onPressed: () {
-              CodeConRepository().getPaymentMethods().then(
+              CodeConRepository()
+                  .createReservation(
+                      params: CreateReservationParams(
+                          paymentMethod: 'VA',
+                          orderId: 'ORD-123888',
+                          customerName: 'John Wick',
+                          customerEmail: 'wick@blackpink.com',
+                          returnUrl: '',
+                          totalPayment: 350000))
+                  .then(
                 (value) {
                   switch (value) {
                     case Success(:var data):
-                      for (final method in data) {
-                        log(method.name);
+                      {
+                        log(data.orderId);
+                        log(data.email);
+                        log(data.name);
+                        log(data.status);
+                        log(data.paymentUrl);
                       }
                     case Failure(:var message):
                       log(message);
