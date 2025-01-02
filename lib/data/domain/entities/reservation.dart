381683df68cb764c4c4b9dcc5e9e57d90abe8cfb
Ryansyah <officialteam548@gmail.com>
2025-01-02 18:49:54 +0700

    part 26

---
+++ lib/data/domain/entities/reservation.dart
@@ -0,0 +1,21 @@
+class Reservation {
+  final String orderId;
+  final String email;
+  final String name;
+  final String status;
+  final String paymentUrl;
+
+  Reservation._(
+      {required this.orderId,
+      required this.email,
+      required this.name,
+      required this.status,
+      required this.paymentUrl});
+
+  factory Reservation.fromJson(Map<String, dynamic> json) => Reservation._(
+      orderId: json['Order ID'].toString(),
+      email: json['Email'].toString(),
+      name: json['Name'].toString(),
+      status: json['Status'].toString(),
+      paymentUrl: json['Payment URL'].toString());
+}
