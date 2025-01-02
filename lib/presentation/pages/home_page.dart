28886260d6cf97cf422cddea574b1990cda6d31a
Ryansyah <officialteam548@gmail.com>
2025-01-02 19:47:26 +0700

    part 34

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,8 +1,7 @@
-import 'dart:developer';
-
-import 'package:webinar_fe/data/code_con_repository.dart';
-import 'package:webinar_fe/domain/entities/tresult.dart';
+import 'package:code_con_course_fe/presentation/constants.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/code_con_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,28 +9,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              CodeConRepository().checkReservation('wick@blackpink.com').then(
-                (value) {
-                  switch (value) {
-                    case Success(:var data):
-                      {
-                        log(data.orderId);
-                        log(data.email);
-                        log(data.name);
-                        log(data.status);
-                        log(data.paymentUrl);
-                      }
-                    case Failure(:var message):
-                      log(message);
-                  }
-                },
-              );
-            },
-            child: const Text('Click Me!')),
-      ),
+      backgroundColor: tertiaryColor,
+      appBar: const PreferredSize(
+          preferredSize: Size(double.infinity, 60), child: CodeConAppBar()),
+      body: SafeArea(
+          child: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+          ListView(
+            children: const [
+              Text('HomePageHeader'),
+              sizedBoxHeight60,
+              Text('BriefDescriptionSection'),
+              Text('SpeakerSection'),
+              Text('AgendaSection'),
+              sizedBoxHeight60,
+              Text('LinksSection'),
+              Text('FooterSection'),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
