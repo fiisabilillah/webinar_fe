0650e6b6c4b9766d9def29142ba702a783ea2188
Ryansyah <officialteam548@gmail.com>
2025-01-03 12:05:17 +0700

    perbaikan tempat file

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -4,8 +4,8 @@ import 'package:webinar_fe/presentation/widgets/brief_description_section.dart';
 import 'package:webinar_fe/presentation/widgets/home_page_header.dart';
 import 'package:flutter/material.dart';
 
-import '../providers/speaker_section.dart';
 import '../widgets/code_con_app_bar.dart';
+import '../widgets/speaker_section.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
